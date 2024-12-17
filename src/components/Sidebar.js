a4bca3b6fb991f10b1db7ee36d98a71e0b33cd05
paullmullen <paulmullen@wi.rr.com>
2024-12-16 21:58:47 -0600

    Issues log working

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import { SidebarData } from './SidebarData';
-import SubMenu from './SubMenu';
-import { IconContext } from 'react-icons/lib';
-
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { SidebarData } from "./SidebarData";
+import SubMenu from "./SubMenu";
+import { IconContext } from "react-icons/lib";
 
 const Nav = styled.div`
   background: #15171c;
@@ -39,7 +38,6 @@ export const SidebarNav = styled.div`
   transition: 0.3s ease-in-out;
 `;
 
-
 const SidebarWrap = styled.div`
   width: 100%;
 `;
@@ -48,7 +46,7 @@ const Sidebar = () => {
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => {
     setSidebar(!sidebar);
-  }
+  };
 
   return (
     <>
@@ -66,9 +64,9 @@ const Sidebar = () => {
             </NavIcon>
             {SidebarData.map((item, index) => {
               return (
-                <SubMenu 
-                  item={item} 
-                  key={index} 
+                <SubMenu
+                  item={item}
+                  key={index}
                   onClick={() => setSidebar(false)} // Close the sidebar on item click
                 />
               );
--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,54 +1,57 @@
-import React from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import * as IoIcons from 'react-icons/io';
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
 
 export const SidebarData = [
   {
-    title: 'Recipes',
-    path: 'Recipes',
+    title: "Recipes",
+    path: "Recipes",
     icon: <AiIcons.AiFillHome />,
-
   },
   {
-    title: 'Reference',
-    path: 'Reference',
+    title: "Reference",
+    path: "Reference",
     icon: <IoIcons.IoIosPaper />,
-  
   },
   {
-    title: 'Inventory',
-    path: 'Inventory',
-    icon: <FaIcons.FaCartPlus />
+    title: "Inventory",
+    path: "Inventory",
+    icon: <FaIcons.FaCartPlus />,
   },
   {
-    title: 'Check Lists',
-    path: 'Checklists',
-    icon: <IoIcons.IoMdPeople />
+    title: "Check Lists",
+    path: "Checklists",
+    icon: <IoIcons.IoMdPeople />,
   },
   {
-    title: 'Special Events',
-    path: 'Specials',
-    icon: <IoIcons.IoMdPeople />
+    title: "Special Events",
+    path: "Specials",
+    icon: <IoIcons.IoMdPeople />,
   },
   {
-    title: 'Inventory',
-    path: 'Inventory',
+    title: "Inventory",
+    path: "Inventory",
     icon: <FaIcons.FaEnvelopeOpenText />,
   },
   {
-    title: 'Maintenance',
-    path: 'Maintenance',
-    icon: <IoIcons.IoMdHelpCircle />
-  },  
-  {
-    title: 'Safety Log',
-    path: 'SafetyLog',
-    icon: <IoIcons.IoMdHelpCircle />
+    title: "Maintenance",
+    path: "Maintenance",
+    icon: <IoIcons.IoMdHelpCircle />,
   },
   {
-    title: 'Drawer Calculator',
-    path: 'Calculator',
-    icon: <IoIcons.IoMdHelpCircle />
+    title: "Safety Log",
+    path: "SafetyLog",
+    icon: <IoIcons.IoMdHelpCircle />,
+  },
+  {
+    title: "Drawer Calculator",
+    path: "Calculator",
+    icon: <IoIcons.IoMdHelpCircle />,
+  },
+  {
+    title: "Feedback Log",
+    path: "FeedbackList",
+    icon: <IoIcons.IoMdHelpCircle />,
   },
 ];
